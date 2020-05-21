a927d14929eb0f88efb3cfcb91ed53318fb313c1
cristhian2019 <cristhian@idp.com>
2020-05-20 22:42:06 -0500

    Diseño dashboard modificado

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,39 @@
 <template>
     <v-app>
 		<!-- Barra de navegación -->
-		<v-navigation-drawer app color="lania">
+		<v-navigation-drawer app color="blue-grey darken-4">
 			<v-list-item>
-				<v-list-item-content class="titulo">
+				<v-list-item-content class="itemslist">
 					<v-list-item-title>
 						<h1> Movies Store </h1>
 					</v-list-item-title>
 				</v-list-item-content>
 			</v-list-item>
 	
-			<v-divider></v-divider>
+
 	
 			<v-list dense nav>
 				<v-list-item v-for="item in items" :key="item.title" link>
 					<v-list-item-icon>
-						<v-icon color="enlaces">{{ item.icon }}</v-icon>
+						<v-icon>{{ item.icon }}</v-icon>
 					</v-list-item-icon>
-		
-					<v-list-item :to="item.route" color="enlaces">
-						<v-list-item-title class="titulo">{{ item.title }}</v-list-item-title>
+
+					<v-list-item :to="item.route" color="indigo">
+						<v-list-item-title class="itemslist">{{ item.title }}</v-list-item-title>
 					</v-list-item>
 				</v-list-item>
 			</v-list>
 		</v-navigation-drawer>
 
 		<!-- Barra de título -->
-		<v-app-bar app color="indigo">
+		<v-app-bar app color="deep-purple darken-4">
 			<v-toolbar-title class="headline text-uppercase">
-				<span></span>
+				<span> </span>
 			</v-toolbar-title>
 			<v-spacer></v-spacer>
 		</v-app-bar>
 
-		<v-content>
+		<v-content class="contenido">
 			<router-view/>
 		</v-content>
 		
@@ -55,3 +55,12 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+.itemslist{
+	color: azure;
+}
+.contenido{
+	background-color: whitesmoke;
+}
+</style>
