152815b3514facbfa0923e87504e0a617632ec46
cristhian2019 <cristhian@idp.com>
2020-06-01 13:39:12 -0500

    Modal para ver trailer de pelicula

--- src/components/TablaPeliculas.vue
+++ src/components/TablaPeliculas.vue
@@ -1,10 +1,34 @@
 <template>
     <v-container fluid class="cont">
         <v-row justify="center">
+            <v-dialog v-model="dialog2" max-width="800px" persistent class="ModalTrailer">
+                <v-card class="ModalTrailer">
+                    <v-toolbar dark color="#1A242E">
+                    <v-btn icon dark @click="dialog2=false">
+                        <v-icon>mdi-close</v-icon>
+                    </v-btn>
+                    <v-toolbar-title> Tráiler </v-toolbar-title>
+                    <v-spacer></v-spacer>
+                    <v-toolbar-items>
+                        
+                    </v-toolbar-items>
+                    </v-toolbar>
+                    <v-card-text>
+                        <video 
+                        class="vtrailer"
+                        :src= direccion
+                        autoplay
+                        controls
+                        ></video>
+                    </v-card-text>
+                </v-card>
+            </v-dialog>
+        </v-row>
+        <v-row justify="center">
             <v-dialog v-model="dialog" persistent  dark="" max-width="600px">
             <v-card>
                     <v-toolbar dark color="primary">
-                    <v-btn icon dark @click="dialog = false">
+                    <v-btn icon dark @click="cerrar()">
                         <v-icon>mdi-close</v-icon>
                     </v-btn>
                     <v-toolbar-title> Mas información de la pelicula {{ peli.title }} </v-toolbar-title>
@@ -17,10 +41,19 @@
                         <v-container>
                             <v-row>
                                 <v-col cols="12">
+                                    <h3> Reparto </h3>
+                                </v-col>
+                                <v-col cols="4" v-for="a in reparto" :key="a">
+                                    <v-img
+                                    :src= a.imagen>
+                                    </v-img>
+                                    <span class="text-center"> {{ a.nombre }} </span>
+                                </v-col>
+                                <v-col cols="12">
                                     <h1 class="text-center"> Resumen </h1>
                                 </v-col>
                                 <v-col cols="12">
-                                    <p class="text-center">
+                                    <p class="text-justify">
                                         {{ peli.descripcion }}
                                     </p>
                                 </v-col>
@@ -78,8 +111,9 @@
                             <v-btn
                             color="white"
                             text
+                            @click="verTrailer(n.trailer)"
                             >
-                            Ver
+                            Ver Tráiler
                             </v-btn>
                         </v-card-actions>
                 </v-card>
@@ -90,41 +124,25 @@
 
 <script>
 import axios from 'axios'
+import { mapState, mapMutations } from 'vuex'
 export default {
     name: 'TablaPeliculas',
     data() {
         return {
-            peliculas: [],
-            generos: [],
             peli: {
                 'title':'',
                 'descripcion': ''
             },
             select: 0,
             dialog: false,
+            dialog2: false,
             title: "",
+            direccion: "",
             descripcion: "",
+            reparto: [],
         }
     },
     methods: {
-        ObtenerPeliculas() {
-            const path = 'http://127.0.0.1:8000/api/peliculas/'
-            axios.get(path).then((response) => {
-                this.peliculas = response.data
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-        },
-        obtenerGeneros() {
-            const path = 'http://127.0.0.1:8000/api/generos/'
-            axios.get(path).then((response) => {
-                this.generos = response.data
-            })
-            .catch((error)=>{
-                console.log(error)
-            })
-        },
         obtenerPeliculasporGenero() {
             const path = `http://127.0.0.1:8000/api/generos/peliculas/${this.select}`
             axios.get(path).then((response) => {
@@ -134,15 +152,37 @@ export default {
                 console.log(error)
             })
         },
+        obtenerReparto(id){
+            const path= `http://127.0.0.1:8000/api/actores/pelicula/${id}`
+            axios.get(path).then((response) => {
+                this.reparto = response.data
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+        },
+        verTrailer(dir) {
+            this.dialog2=true;
+            this.direccion= dir;
+        },
         c(n) {
             this.dialog= true;
             this.peli.title= n.titulo;
             this.peli.descripcion = n.descripcion;
-        }
+            this.obtenerReparto(n.id);
+        },
+        cerrar() {
+            this.dialog=false;
+            this.reparto=[];
+        },
+        ...mapMutations(['ObtenerPeliculas','ObtenerGeneros'])
     },
     created () {
         this.ObtenerPeliculas();
-        this.obtenerGeneros();
+        this.ObtenerGeneros();
+    },
+    computed: {
+        ...mapState(['peliculas','generos'])
     }
 }
 </script>
@@ -150,5 +190,11 @@ export default {
 <style scoped>
 .select{
     color: antiquewhite;
+}
+.ModalTrailer{
+    background-color: transparent;
+}
+.vtrailer{
+    max-width: 750px;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,34 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    peliculas: [],
+    generos: []
   },
   mutations: {
+    //Función que solicitará las peliculas a la API
+      ObtenerPeliculas: function(state) {
+        const path = 'http://127.0.0.1:8000/api/peliculas/'
+        axios.get(path).then((response) => {
+            state.peliculas = response.data
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    },
+    //Función que solicitará los generos a la API
+      ObtenerGeneros: function(state){
+        const path = 'http://127.0.0.1:8000/api/generos/'
+            axios.get(path).then((response) => {
+                state.generos = response.data
+            })
+            .catch((error)=>{
+                console.log(error)
+            })
+      }
   },
   actions: {
   },
